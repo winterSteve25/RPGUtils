f44c024c875833df0cae8c26676ebc570b1732a8
Contrabass26 <jsedwards89@hotmail.co.uk>
2022-08-31 09:12:41 +0100

    Added variable NPC models and animations

--- src/main/java/wintersteve25/rpgutils/client/renderers/npc/NPCModel.java
+++ src/main/java/wintersteve25/rpgutils/client/renderers/npc/NPCModel.java
@@ -1,11 +1,37 @@
 package wintersteve25.rpgutils.client.renderers.npc;
 
-import wintersteve25.rpgutils.client.renderers.GeckolibModelBase;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import wintersteve25.rpgutils.RPGUtils;
+import wintersteve25.rpgutils.common.data.loaded.npc.datum_type.StringNPCDatumType;
 import wintersteve25.rpgutils.common.entities.NPCEntity;
+import wintersteve25.rpgutils.common.entities.NPCType;
 
-public class NPCModel extends GeckolibModelBase<NPCEntity> {
+public class NPCModel extends AnimatedGeoModel<NPCEntity> {
 
-    public NPCModel() {
-        super("geo/npc.geo.json", "textures/entity/npc/npc.png", "animations/npc.animation.json");
+    private static final ResourceLocation DEFAULT_PATH = new ResourceLocation(RPGUtils.MOD_ID, "geo/npc.geo.json");
+    private static final ResourceLocation DEFAULT_ANIMATIONS = new ResourceLocation(RPGUtils.MOD_ID, "animations/npc.animation.json");
+
+    @Override
+    public ResourceLocation getModelLocation(NPCEntity entity) {
+        NPCType type = entity.getNPCType();
+        if (type != null) {
+            return new ResourceLocation(RPGUtils.MOD_ID, type.getDatum(StringNPCDatumType.MODEL));
+        }
+        return DEFAULT_PATH;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(NPCEntity entity) {
+        return new ResourceLocation(RPGUtils.MOD_ID, entity.getTexturePath());
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(NPCEntity entity) {
+        NPCType type = entity.getNPCType();
+        if (type != null) {
+            return new ResourceLocation(RPGUtils.MOD_ID, type.getDatum(StringNPCDatumType.ANIMATIONS));
+        }
+        return DEFAULT_ANIMATIONS;
     }
 }
